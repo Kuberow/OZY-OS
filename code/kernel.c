f54ee9a9a5317f95c397a392395647243ce166cc
Kuberow <kuberow.kuberow@gmail.com>
2025-08-15 14:29:04 +0300

    Fix kernel.c

--- code/kernel.c
+++ code/kernel.c
@@ -1,7 +1,29 @@
 // C kernel here:
-#include <stdio.h>
+__attribute__((section(".text"))) void kmain(void);
 
-int main(void) {
-    printf("Hello world\n");
-    return 0;
+static inline void vm_putchar(char c) {
+    // Replace this with your VM’s syscall or console write
+    __asm__ volatile ("int $0x10" : : "a"(0x0E00 | c));
 }
+
+void println(const char *s) {
+    while (*s) vm_putchar(*s++);
+}
+
+void kmain(void) {
+    println("Hello from my 32-bit OS!");
+    for(;;) __asm__("hlt");
+}
+
+__asm__(
+".global _start\n"
+"_start:\n"
+"cli\n"
+"xor %ax,%ax\n"
+"mov %ax,%ds\n"
+"mov %ax,%es\n"
+"mov %ax,%ss\n"
+"mov $0x90000,%sp\n"   // simple stack
+"call kmain\n"
+"hlt\n"
+);
