688b2b0fce77205ea42eefe4bd25d563535a36ac
Kuberow <kuberow.kuberow@gmail.com>
2025-08-16 10:56:47 +0300

    Update kernel.c

--- code/kernel.c
+++ code/kernel.c
@@ -1,45 +1,25 @@
-// C kernel here:
-__attribute__((section(".text"))) void kmain(void);
+// prog.c
+// Simple program for your ARMvCPU emulator
+// Provides print() using svc #0 as putchar(R0)
 
-// VM syscall: output one character
-static inline void vm_putchar(char c) {
-    // Your VM uses "OUT R0" style instruction; we'll pretend the compiler knows it
-    // For your VM, replace this with the opcode manually if needed
-    __asm__ volatile(
-        "movi r0, %0\n\t"
-        "out r0\n"
-        :
-        : "r"(c)
-    );
+static inline void svc_putchar(char c) {
+    register int r0 __asm__("r0") = (unsigned char)c;
+    __asm__ __volatile__("svc #0" : "+r"(r0));
 }
 
-// Print a string
 void print(const char *s) {
     while (*s) {
-        vm_putchar(*s++);
+        svc_putchar(*s++);
     }
 }
 
-// Print a string + newline
-void println(const char *s) {
-    print(s);
-    vm_putchar('\n');
+__attribute__((naked)) void _start(void) {
+    __asm__ __volatile__(
+        "ldr sp, =0x00100000\n" // set up a stack
+    );
+
+    print("Hello, World!\n");
+    print("- Kuberow")
+
+    __asm__ __volatile__("mov pc, lr"); // exit: PC = LR (your VM halts)
 }
-
-// Kernel entry
-void kmain(void) {
-    println("Hello from my 32-bit VM!");
-    println("It stops here.");
-
-    // Halt the VM
-    for(;;) __asm__ volatile("halt"); // infinite halt loop
-}
-
-// Startup assembly: call kmain
-__asm__ (
-".global _start\n"
-"_start:\n"
-"    mov $0x90000, %sp\n" // simple stack
-"    call kmain\n"
-"    hlt\n"
-);
