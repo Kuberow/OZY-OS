530551e466c562220ef3bd3e013dca2552c40f6f
Kuberow <kuberow.kuberow@gmail.com>
2025-08-16 11:06:56 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,22 +5,19 @@
 SRC="code/kernel.c"
 
 # Name of output file
-# ELF="build/kernel.elf"
+ELF="build/kernel.elf"
 BIN="build/kernel.bin"
 
 # Clean previous builds
 rm $BIN
 
-# Compile to 32-bit object (freestanding optional)
+# Step 1: compile and link to ELF
 arm-none-eabi-gcc -nostdlib -ffreestanding -Os -marm -mcpu=arm7tdmi \
-  -Wl,-Ttext=0x0 -Wl,--oformat=binary code/kernel.c -o build/kernel.bin
+  -Wl,-Ttext=0x0 \
+  -o $ELF $SRC
 
-
-# Link to ELF starting at 0x10000 (adjust if your VM wants a different address)
-# ld -m elf_i386 -Ttext 0x10000 -o $ELF main.o -e main
-
-# Convert ELF to flat binary
-# objcopy -O binary $ELF $BIN
+# Step 2: convert ELF to raw binary
+arm-none-eabi-objcopy -O binary $ELF $BIN
 cp vm.lua build/vm.lua
 cp startup.lua build/startup.lua
 
