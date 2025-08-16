94dde1f44e8e475e5b69c601c9edb750c0b93a3a
Kuberow <kuberow.kuberow@gmail.com>
2025-08-16 22:34:07 +0300

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,1 +1,9 @@
-print("still work in progress, may release in a week or less.")
+x, y = term.getSize()
+term.clear()
+paintutils.drawFilledBox(1, 1, x, y, colors.red)
+term.setCursorPos(1,1)
+print("DUDE! This is not available yet. still work in progress by me Kuberow and Tom!")
+print("Terminate to go back!")
+while true do
+sleep(0.1)
+end
