36de6319d5b7986d8197dba198b5c425f7d88d93
Kuberow <kuberow.kuberow@gmail.com>
2025-08-17 17:23:34 +0300

    Update LCFO.sh

--- sdk/LCFO.sh
+++ sdk/LCFO.sh
@@ -1,5 +1,6 @@
-echo "if it errors usage: LCFO output.ozex input.lua"
+echo "if it errors usage: LCFO output.ozex input.lua <OZLE/OZEX>"
+echo "OZLE format is Lite and OZEX format is Normal!"
 luac -o bin.luac $2
-echo -n "OZEX" > $1
+echo -n $3 > $1
 cat bin.luac >> $1
 echo "Compiled!"
