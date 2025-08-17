a9c19cbc3d5eafd8bf311921c066ee27c39c63fe
Kuberow <kuberow.kuberow@gmail.com>
2025-08-17 17:21:11 +0300

    Update kernel.lua

--- code/kernel.lua
+++ code/kernel.lua
@@ -18,6 +18,33 @@ function line(c, user, loc)
 end
 
 function interpret(inc)
-  
+  if fs.exists("/bin/"..inc) then
+    local filename = inc  -- your custom executable
+
+-- open the file in binary mode
+local file = fs.open(filename, "rb")
+if not file then error("Cannot open file: "..filename) end
+
+local content = file.readAll()
+file.close()
+
+-- check the header
+local header = string.sub(content, 1, 4)
+if header ~= "OZLE" then
+    term.write("Invalid format: ")
+    if header == "OZEX" then
+    term.write("Not a Lite Format!")
+    end
+end
+
+-- get the Lua bytecode part
+local bytecode = string.sub(content, 5)
+
+-- load and run the bytecode
+local f, err = load(bytecode)
+if not f then error("Failed to load bytecode: "..err) end
+f()
+  else
+  end
 end
 
