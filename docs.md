1aa471a3605b118c25c7fc6701626f3083636f7b
Kuberow <kuberow.kuberow@gmail.com>
2025-08-18 11:41:39 +0300

    Update and rename sdk/LCFO.sh to docs.md

---
+++ docs.md
@@ -0,0 +1,2 @@
+# Creating Public Distros
+to create your own distro, fork this repo and modify what you want (modify the installer to work)
--- sdk/LCFO.sh
+++
@@ -1,6 +0,0 @@
-echo "if it errors usage: LCFO output.ozex input.lua <OZLE/OZEX>"
-echo "OZLE format is Lite and OZEX format is Normal!"
-luac -o bin.luac $2
-echo -n $3 > $1
-cat bin.luac >> $1
-echo "Compiled!"
