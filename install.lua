4e8c406c488f1922180ee30a3177fa8aa9a5ebb3
Kuberow <kuberow.kuberow@gmail.com>
2025-08-19 16:27:31 +0300

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,9 +1,9 @@
-shell.run("wget", "https://raw.github.com/Kuberow/OZY-OS-Lite/main/code/kernel.lua", "/kernel.lua")
-shell.run("wget", "https://raw.github.com/Kuberow/OZY-OS-Lite/main/code/bootloader/main.lua", "/startup.lua")
+shell.run("wget", "https://raw.github.com/Kuberow/OZY-OS/main/code/kernel.lua", "/kernel.lua")
+shell.run("wget", "https://raw.github.com/Kuberow/OZY-OS/main/code/bootloader/main.lua", "/startup.lua")
 print("Core Installed.")
 sleep(2)
 fs.makeDir("bin")
-shell.run("wget", "https://raw.github.com/Kuberow/OZY-OS-Lite/main/defcmd/help", "/bin/help")
-shell.run("wget", "https://raw.github.com/Kuberow/OZY-OS-Lite/main/defcmd/ozp", "/bin/ozp")
+shell.run("wget", "https://raw.github.com/Kuberow/OZY-OS/main/defcmd/help", "/bin/help")
+shell.run("wget", "https://raw.github.com/Kuberow/OZY-OS/main/defcmd/ozp", "/bin/ozp")
 print("Fully Installed.")
 
